4efc13a0d9fe2b300622d1d4e915b7a5204f24a5
Tonyn125 <tonyn1@student.unimelb.edu.au>
2017-10-26 23:17:32 +1100

    read.md

--- app.py
+++ app.py
@@ -90,4 +90,4 @@ def about():
 	return render_template("observations.html") + render_template("footer.html")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=80)
+    app.run(debug=True, host='127.0.0.1', port=3001)
